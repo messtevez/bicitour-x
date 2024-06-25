ffd12031e9ad827640e9cb58f8019d8750b22167
Maria Estèvez <m.estcasas@gmail.com>
2024-06-24 21:38:35 -0500

    mediaQueries

---
+++ assets/js/formulario.js
@@ -0,0 +1,5 @@
+const nombre = document.getElementById(nombre)
+const email = document.getElementById(email)
+const telefono = document.getElementById(telefono)
+const opciones = document.getElementById(opciones)
+
