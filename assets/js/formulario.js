01fabfc5d15d1adf594026f10324a8371cf0c21e
Maria Estèvez <m.estcasas@gmail.com>
2024-06-25 17:56:02 -0500

    js

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,5 +1,22 @@
-const nombre = document.getElementById(nombre)
-const email = document.getElementById(email)
-const telefono = document.getElementById(telefono)
-const opciones = document.getElementById(opciones)
+const nombre = document.getElementById('nombre')
+const spanNombre = document.getElementById('span-name')
+const email = document.getElementById('email')
+const telefono = document.getElementById('telefono')
+const opciones = document.getElementById('opciones')
+const boton = document.getElementById('boton')
 
+if(nombre.value = ''){
+    nombre.style.border = '2px solid red'
+    spanNombre.textContent = 'Hey!! el campo nombre es necesario!!'
+}
+
+boton.addEventListener('click', (evento) => {
+    if(nombre.value == ''){
+        console.log('Entro')
+        nombre.style.border = '2px solid red'
+        spanNombre.textContent = 'Hey!! el campo nombre es necesario!!'
+    }else {
+        nombre.style.border = 'none'
+        spanNombre.textContent = ''
+    }
+})
